1da21844fef886746ea5c69ef46e3f4b40fff9ba
THUC <THUC@DESKTOP-92NV87C>
2025-08-28 16:59:28 +0700

    dd

--- TApp/MainForm.Designer.cs
+++ TApp/MainForm.Designer.cs
@@ -47,7 +47,7 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(880, 463);
+            ClientSize = new Size(809, 649);
             Controls.Add(uiSymbolButton1);
             Name = "MainForm";
             Text = "MainForm";
