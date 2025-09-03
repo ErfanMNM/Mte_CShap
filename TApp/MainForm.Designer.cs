310035c93004c1a6070e7887ea02a454e817b15c
DANOMT <DANOMT@DESKTOP-6PD1UF7>
2025-09-03 22:52:34 +0700

    `d

--- TApp/MainForm.Designer.cs
+++ TApp/MainForm.Designer.cs
@@ -29,9 +29,9 @@
         private void InitializeComponent()
         {
             uiTableLayoutPanel1 = new Sunny.UI.UITableLayoutPanel();
-            NavMenu = new Sunny.UI.UINavMenu();
-            MainTab = new Sunny.UI.UITabControl();
             uiTableLayoutPanel2 = new Sunny.UI.UITableLayoutPanel();
+            MainTab = new Sunny.UI.UITabControl();
+            NavMenu = new Sunny.UI.UINavMenu();
             uiTableLayoutPanel1.SuspendLayout();
             uiTableLayoutPanel2.SuspendLayout();
             SuspendLayout();
@@ -53,6 +53,42 @@
             uiTableLayoutPanel1.TabIndex = 2;
             uiTableLayoutPanel1.TagString = null;
             // 
+            // uiTableLayoutPanel2
+            // 
+            uiTableLayoutPanel2.ColumnCount = 2;
+            uiTableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 16.094986F));
+            uiTableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 83.905014F));
+            uiTableLayoutPanel2.Controls.Add(MainTab, 1, 0);
+            uiTableLayoutPanel2.Controls.Add(NavMenu, 0, 0);
+            uiTableLayoutPanel2.Dock = DockStyle.Fill;
+            uiTableLayoutPanel2.Location = new Point(0, 43);
+            uiTableLayoutPanel2.Margin = new Padding(0);
+            uiTableLayoutPanel2.Name = "uiTableLayoutPanel2";
+            uiTableLayoutPanel2.RowCount = 1;
+            uiTableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            uiTableLayoutPanel2.Size = new Size(1137, 556);
+            uiTableLayoutPanel2.TabIndex = 2;
+            uiTableLayoutPanel2.TagString = null;
+            // 
+            // MainTab
+            // 
+            MainTab.DrawMode = TabDrawMode.OwnerDrawFixed;
+            MainTab.FillColor = Color.FromArgb(192, 255, 255);
+            MainTab.Font = new Font("Microsoft Sans Serif", 12F);
+            MainTab.ItemSize = new Size(150, 40);
+            MainTab.Location = new Point(185, 2);
+            MainTab.MainPage = "";
+            MainTab.Margin = new Padding(2);
+            MainTab.MenuStyle = Sunny.UI.UIMenuStyle.Custom;
+            MainTab.Name = "MainTab";
+            MainTab.SelectedIndex = 0;
+            MainTab.Size = new Size(950, 550);
+            MainTab.SizeMode = TabSizeMode.Fixed;
+            MainTab.TabBackColor = Color.FromArgb(192, 255, 255);
+            MainTab.TabIndex = 0;
+            MainTab.TabUnSelectedForeColor = Color.FromArgb(240, 240, 240);
+            MainTab.TipsFont = new Font("Microsoft Sans Serif", 9F);
+            // 
             // NavMenu
             // 
             NavMenu.BackColor = Color.Teal;
@@ -72,42 +108,6 @@
             NavMenu.Size = new Size(179, 550);
             NavMenu.TabIndex = 1;
             NavMenu.TipsFont = new Font("Microsoft Sans Serif", 9F);
-            // 
-            // MainTab
-            // 
-            MainTab.DrawMode = TabDrawMode.OwnerDrawFixed;
-            MainTab.FillColor = Color.FromArgb(192, 255, 255);
-            MainTab.Font = new Font("Microsoft Sans Serif", 12F);
-            MainTab.ItemSize = new Size(150, 40);
-            MainTab.Location = new Point(185, 2);
-            MainTab.MainPage = "";
-            MainTab.Margin = new Padding(2);
-            MainTab.MenuStyle = Sunny.UI.UIMenuStyle.Custom;
-            MainTab.Name = "MainTab";
-            MainTab.SelectedIndex = 0;
-            MainTab.Size = new Size(948, 550);
-            MainTab.SizeMode = TabSizeMode.Fixed;
-            MainTab.TabBackColor = Color.FromArgb(192, 255, 255);
-            MainTab.TabIndex = 0;
-            MainTab.TabUnSelectedForeColor = Color.FromArgb(240, 240, 240);
-            MainTab.TipsFont = new Font("Microsoft Sans Serif", 9F);
-            // 
-            // uiTableLayoutPanel2
-            // 
-            uiTableLayoutPanel2.ColumnCount = 2;
-            uiTableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 16.094986F));
-            uiTableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 83.905014F));
-            uiTableLayoutPanel2.Controls.Add(MainTab, 1, 0);
-            uiTableLayoutPanel2.Controls.Add(NavMenu, 0, 0);
-            uiTableLayoutPanel2.Dock = DockStyle.Fill;
-            uiTableLayoutPanel2.Location = new Point(0, 43);
-            uiTableLayoutPanel2.Margin = new Padding(0);
-            uiTableLayoutPanel2.Name = "uiTableLayoutPanel2";
-            uiTableLayoutPanel2.RowCount = 1;
-            uiTableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
-            uiTableLayoutPanel2.Size = new Size(1137, 556);
-            uiTableLayoutPanel2.TabIndex = 2;
-            uiTableLayoutPanel2.TagString = null;
             // 
             // MainForm
             // 
